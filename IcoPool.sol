bdee60597cba5e6a50c71fe7994c7d4fa1a8da8b
Faeez Shaikh <faeez.shaikh@gmail.com>
2018-01-27 19:26:49 -0600

    Updates

--- IcoPool.sol
+++ IcoPool.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.18;
 
+import "./ERC20Interface.sol";
 contract IcoPool {
     
     address icoContract;
@@ -9,13 +10,29 @@ contract IcoPool {
     uint minPerContributor;
     address[] admins;
     address[] whitelist;
-    address[] contributors;
+    mapping (address => bool) contributors;
+    address[] contributorsList;
     uint fee;
     bool automaticDistribution;
+    
+    struct erc20token {
+        address tokenAddress;
+        string name;
+    }
+    mapping (address => uint) tokenBalanceForAddress;
+    
+    ERC20Interface token;
    
     mapping (address => uint) contributions;
     
+    modifier onlyPoolCreator() {
+        if (msg.sender == poolCreator) {
+            _;
+        }
+    }
+    
     event withdrawal(address contributor, uint amount);
+    event DepositForTokenReceived(address, uint, uint);
     
     function IcoPool( uint maxPoolAllocation1, uint maxPerContributor1, uint minPerContributor1) public {
         poolCreator = msg.sender;
@@ -24,6 +41,7 @@ contract IcoPool {
         maxPoolAllocation = maxPoolAllocation1;
     }
     
+    //// Getters /////
     
     function getMaxPoolAllocation() constant public returns(uint) {
         return maxPoolAllocation;
@@ -43,24 +61,46 @@ contract IcoPool {
      function getPoolCreator() constant public returns(address) {
         return poolCreator;
     }
+    
+     function getContributors() public constant returns(address[]) {
+        return contributorsList;
+    }
+    function getMyContribution() public constant returns (uint) {
+        return contributions[msg.sender];
+    }
+    function getContribution(address addr) public constant returns (uint) {
+        return contributions[addr];
+    }
+      
+    function getRegisteredTokenSupply() public constant returns (uint) {
+        return token.totalSupply();
+    }
+    
+      
+    function getTokenBalance(address addr) public constant returns (uint) {
+        return tokenBalanceForAddress[addr];
+    }
+    ////// Setters ////////
+    
       function setMinPerContributorn(uint contribution) public {
           require(contribution < maxPerContributor);
          minPerContributor = contribution;
     }
     
     function contribute() public payable {
-        contributions[msg.sender] = msg.value;
+        contributions[msg.sender] += msg.value;
         
-        // is there a way to retrieve keys from mapping?
-        contributors.push(msg.sender);
+           if(!alreadyContributed(msg.sender)) contributorsList.push(msg.sender); 
+           contributors[msg.sender] = true;
     }
     
-    function getContributors() public constant returns(address[]) {
-        return contributors;
+    
+    function alreadyContributed(address addr) public constant returns (bool) {
+        if(contributors[addr]) return true;
+        return false;
     }
-    function getContribution() public constant returns (uint) {
-        return contributions[msg.sender];
-    }
+    
+   
     
     function withdrawContribution(uint amount) public  returns (bool) {
         require(contributions[msg.sender] > 0);
@@ -69,7 +109,27 @@ contract IcoPool {
         
         contributions[msg.sender] -= amount;
         msg.sender.transfer(amount);
+        
+        if(contributions[msg.sender] == 0) contributors[msg.sender] = false;
         withdrawal(msg.sender,amount);
         return true;
     }
+    
+    function registerToken(address tokenAddress) public onlyPoolCreator returns (bool){
+        require(tokenAddress != address(0));
+        token = ERC20Interface(tokenAddress);
+        return true;
+    }
+  
+    
+    // Token owner most likely invokes this method. msg.sender is the token owner that calls this function
+     function depositToken(uint amount) public returns (bool) {
+        require(token.transferFrom(msg.sender, address(this), amount) == true);
+        require(tokenBalanceForAddress[poolCreator] + amount >= tokenBalanceForAddress[poolCreator]);
+        tokenBalanceForAddress[poolCreator] += amount;
+        DepositForTokenReceived(poolCreator, amount, now);
+        return true;
+    }
+  
+  // 0xf828377c9048dac1e072734cf1ffc01db5887758
 }
